04bf706eb4369f4bd9a57d68d9e6f41c61d72452
wangs <wangs@yindou.com>
2018-08-16 22:00:00 +0800

    init

--- app/src/main/java/com/moveimageview/MainActivity.java
+++ app/src/main/java/com/moveimageview/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+//0816
 public class MainActivity extends AppCompatActivity {
 
     @Override
