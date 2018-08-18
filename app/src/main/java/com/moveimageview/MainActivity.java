21d09fbb67f8b1edd35dfc713f4299041d47a243
wangs <wangs@yindou.com>
2018-08-18 09:00:27 +0800

    冲突解决

--- app/src/main/java/com/moveimageview/MainActivity.java
+++ app/src/main/java/com/moveimageview/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+
+
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
